fd7f7ccf2527b6845c919618674a12f74ac43e16
anvarganiev <qwerty0704996@gmail.com>
2022-06-06 11:59:57 +0300

    added rgb to grayscale input converting for pretrained models

---
+++ squeeze_input.py
@@ -0,0 +1,25 @@
+import torchvision.models as models
+
+
+def squeeze_weights(model):
+    model.weight.data = model.weight.data.sum(dim=1)[:, None]
+    model.in_channels = 1
+
+
+if __name__ == '__main__':
+    mobilenet_v2 = models.mobilenet_v2()
+    # смотрим, как называется первый сверточный слой
+    print(mobilenet_v2.features[0])
+    # видим ConvNormActivation c входной размерностью 3:
+    # Sequential(
+    # (0): ConvNormActivation(
+    #   (0): Conv2d(3, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
+    # ...
+    # то есть нам нужно взять features[0][0]
+    squeeze_weights(mobilenet_v2.features[0][0])
+    print('\n\nNEW INPUT\n')
+    print(mobilenet_v2.features[0])
+    # теперь видим одноканальный вход
+    # Sequential(
+    #     (0): ConvNormActivation(
+    #     (0): Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
